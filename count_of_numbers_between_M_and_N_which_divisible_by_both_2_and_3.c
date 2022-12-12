803c457bc4b9c66ec353442b7008d79b406a1c43
22A91A61J3 <119840051+22A91A61J3@users.noreply.github.com>
2022-12-12 10:33:09 +0530

    #include<stdio.h> int main() {     int a,b,c=0,i;     scanf("%d%d%d",&a,&b,&i);     for(i=a;i<b;i++)     { if (i%3==0 &&  i%2==0)     {     c++;     }     }printf("%d",c); }

--- count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
+++ count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
@@ -4,9 +4,9 @@ int main()
     int a,b,c=0,i;
     scanf("%d%d%d",&a,&b,&i);
     for(i=a;i<b;i++)
-    { if(i%3==0 && i%2==0)
+    { if (i%3==0 &&  i%2==0)
     {
-        c++;
+    c++;
     }
     }printf("%d",c);
 }
